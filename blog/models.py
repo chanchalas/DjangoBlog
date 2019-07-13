e08bb46be13492db88182b580e51ae96d8d775e2
Chanchala Singh <ChanchalaS@ecmobility.com>
2019-07-13 14:41:27 +0530

    Blog project created succeessfully

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+# Create your models here.
+class BlogPost(models.Model):
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    title  = models.CharField(max_length=180)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return str(self.title)
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'id':self.pk})    
+
+    class Meta:
+        ordering = ['-created_at']
---
+++ blog/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import BlogPost
+from .forms import CreateBlogPostForm
+from django.contrib import messages
+from django.core.paginator import Paginator
+from django.urls import reverse
+
+
+# Create your views here.
+
+def home(request):
+    posts = BlogPost.objects.all()
+    paginator = Paginator(posts, 2)
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+    context = {'posts':posts}
+    return render(request, 'home.html', context)
+
+def detail(request,id):
+    post = get_object_or_404(BlogPost, pk=id)
+    context = {'post':post}
+    return render(request, 'detail.html',context)
+
+def create_post(request):
+    form = CreateBlogPostForm(request.POST or None)
+    if request.POST:
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.author=request.user
+            instance.save()
+            messages.success(request, message=f'{instance.title} post created successfully!!')
+            return redirect('home')
+
+    
+    context = {'form':form}
+    return render(request, 'create.html', context)
+
+
+def edit_post(request, id):
+        post = get_object_or_404(BlogPost, pk=id)
+        form = CreateBlogPostForm(request.POST or None, instance=post)
+        if request.POST:
+            if form.is_valid():
+                instance = form.save()
+                messages.success(request, message=f'{instance.title} Post updated successfully!!')
+                return redirect(reverse('detail',kwargs={'id':instance.pk}))
+        
+        context = {'form':form}
+        return render(request, 'create.html', context)
+
+def delete_post(request,id):
+        post = get_object_or_404(BlogPost, pk=id)
+        if request.POST:
+
+            if post:
+                post.delete()
+                messages.warning(request,message='Post deleted successfully!!!')
+                return redirect('home')   
+        context = {'post':post}  
+        return render(request,'delete.html',context)
