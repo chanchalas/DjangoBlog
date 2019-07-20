1af275516bfd3b49e2d62654b8c783048916e3d0
Chanchala <chanchalasingh111@gmail.com>
2019-07-20 14:30:38 +0530

    changed DEBUG to False

--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!eql)$)i5z-wl^@l2)dabuav3r*hr@$9+-d)33wnlxnu3surpu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['chanchala.herokuapp.com']
 
